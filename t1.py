34576e37b7b3f9f9814ba86ea31fd622e48e30e3
iklob <henrique.cadastro@live.com>
2018-09-04 23:15:23 -0300

    Update t1.py

--- t1.py
+++ t1.py
@@ -7,6 +7,7 @@ Created on 30 de mai de 2018
 import tty
 import sys
 import termios
+import time
 
 orig_settings = termios.tcgetattr(sys.stdin)
 
@@ -16,11 +17,14 @@ first_pressed = None
 second_pressed = None
 
 while key_pressed != chr(27):
+    initial_time = time.time()
     
     if first_pressed is None:
         first_pressed = sys.stdin.read(1)[0]
     
     key_pressed = sys.stdin.read(1)[0]
-    print("\nYou pressed", key_pressed)
-
+    
+    print(key_pressed.replace(' ', ''), 'time:', time.time() - initial_time, type(key_pressed))
+    initial_time = 0
+    
 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings) 
